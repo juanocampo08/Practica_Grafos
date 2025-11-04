c9b2f843f772f41286ce3049282dd2e72ea4b1b5
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-11-04 18:42:57 -0500

    se agrega el metodo agregar sede y el de agregar ruta

--- model/Clase_grafo.py
+++ model/Clase_grafo.py
@@ -1,1 +1,29 @@
-print("Hello, World!")
+
+class Grafo:
+    def __init__(self):
+        self.grafo = {}
+        self.sedes = []
+        self.costo_por_km = 950 
+
+    def agregar_sede(self, nombre_sede):
+        """Agrega una sede al grafo."""
+        if nombre_sede not in self.grafo:
+            self.grafo[nombre_sede] = []
+            self.sedes.append(nombre_sede)
+
+
+    def agregar_ruta(self, sede_1, sede_2, distancia_km):
+        """Agrega una ruta bireccional entre dos sedes.
+        
+        sede_1 y sede_2: nombres de las sedes.
+        distancia_km: distancia en kilómetros entre las sedes.
+        """
+        
+
+grafo_prueba = Grafo()
+grafo_prueba.agregar_sede("Sede a")
+grafo_prueba.agregar_sede("Sede B")
+print(grafo_prueba.grafo)
+
+
+    
