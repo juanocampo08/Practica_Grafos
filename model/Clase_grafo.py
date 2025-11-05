7982585e26094fd58629ad8ba75447671055a302
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-11-04 19:10:28 -0500

    Se completa el metodo de agregar ruta y se termina el metodo que obtiene las rutas de una sede en especifico

--- model/Clase_grafo.py
+++ model/Clase_grafo.py
@@ -18,11 +18,29 @@ class Grafo:
         sede_1 y sede_2: nombres de las sedes.
         distancia_km: distancia en kilómetros entre las sedes.
         """
+        if sede_1 not in self.grafo:
+            self.agregar_sede(sede_1)
+        if sede_2 not in self.grafo:
+            self.agregar_sede(sede_2)
+        
+        costo = distancia_km * self.costo_por_km
+
+        self.grafo[sede_1].append((sede_2, distancia_km, costo))
+        self.grafo[sede_2].append((sede_1, distancia_km, costo))
+
+    def obtener_rutas(self, sede):
+        """Devuelve las rutas desde una sede dada."""
+        return self.grafo.get(sede, [])
+
         
 
 grafo_prueba = Grafo()
-grafo_prueba.agregar_sede("Sede a")
-grafo_prueba.agregar_sede("Sede B")
+
+grafo_prueba.agregar_ruta("sede1", "sede2", 10)
+grafo_prueba.agregar_ruta("sede1", "sede3", 20)
+grafo_prueba.agregar_ruta("sede2", "sede3", 15)
+print(grafo_prueba.obtener_rutas("sede1"))
+print("-"*20)
 print(grafo_prueba.grafo)
 
 
